1f7e5099b965127b29dbbfefad4a6cfebfc18714
Callum O'Riley <callumchristopheroriley@gmail.com>
2022-11-06 10:44:09 -0800

    Update example.py

--- Examples/MySQL connector example/example.py
+++ Examples/MySQL connector example/example.py
@@ -2,18 +2,21 @@ import sys
 import mysql.connector
 import json
 
-f = open(sys.argv[1])
+# run with: python example.py path/to/CREDENTIALS.json
+# See Slack chat for what's in CREDENTIALS.json
 
-credentials = json.load(f)
+f = open(sys.argv[1]) # open credentials file
 
-database = mysql.connector.connect(**credentials["database"])
+credentials = json.load(f) # turn credentials JSON file into a dictionary
+
+database = mysql.connector.connect(**credentials["database"]) # create database object
 cursor = database.cursor()
 
-cursor.execute("SELECT * FROM test_table;")
+cursor.execute("SELECT * FROM test_table;") # get everything in test_table
 for result in cursor:
     print(result)
-#cursor.execute("CREATE TABLE test_table (number1 int, number2 int);")
-cursor.execute("INSERT INTO test_table (number1, number2) VALUES (1, 2);")
+#cursor.execute("CREATE TABLE test_table (number1 int, number2 int);") # create a new table called test_table
+cursor.execute("INSERT INTO test_table (number1, number2) VALUES (1, 2);") # put a value into test_table
 database.commit()
 
 cursor.close()
