4ccfef97b79f18a33e302666c5baee236dd66076
username <charleen.chu3@gmail.com>
2022-11-06 17:36:51 -0800

    dashBoardApp_v1

--- ExamCheck/app.py
+++ ExamCheck/app.py
@@ -14,18 +14,30 @@ def main():
     cursor = database.cursor()
 
     spot_id = request.args.get("spot_id")
-    print(spot_id)
+    #print(spot_id)
 
-    cursor.execute(f"SELECT * FROM exam-data WHERE room_seat_num={int(spot_id)}")
+    cursor.execute(f"SELECT * FROM exam_info WHERE room_seat_num={int(spot_id)}")
+
+    dataDict={}
     for result in cursor:
         print(result)
+        dataDict[result[1]]={"studentName":result[0],
+                            "examName":result[2],
+                            "phoneNum":result[3],
+                            "examTime":(result[4],result[5]),
+                            "item1": result[6],
+                            "item2": result[7],
+                            "item3": result[8]}
 
     if request.method == "POST": # https://www.geeksforgeeks.org/retrieving-html-from-data-using-flask/
         first_name = request.form.get("fname")
         last_name = request.form.get("lname")
         print(first_name)
         print(last_name)
-    return render_template("index.html")
+    return render_template("index.html", data=cursor)
+
+
+
 
 """cursor.close()
 database.close()"""
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# HackCamp 2022 - Henry, Suhail, and Callum
+# HackCamp 2022 - Henry, Suhail,Charleen, and Callum
 HackCamp 2022 - Western Canada's Largest Beginner-Friendly Hackathon
 
 This is our repository for this hackathon!
+
+Charleen Chu: @charleenchu3
